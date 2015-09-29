d030579750831059c19d2e56f11189fb0111bfdc
usuario <usuario@10.22.146.98>
2015-09-29 10:10:24 +0100

    Primera iteración de la práctica 3

---
+++ src/Practica3/panelGrafico.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica3;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author usuario
+ */
+public class panelGrafico extends JPanel{
+    public Double anguloVela=0.0, anguloTimon=0.0;
+    public boolean focusVela=false, focusTimon=false;
+    
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Rectangle rect = this.getBounds();
+        Graphics2D g2 = (Graphics2D)g;
+        
+        g2.setStroke(new BasicStroke(3.0f));
+        g2.drawOval(100, 100, 100, 200);
+        
+        int x1 = 150;
+        int y1 = 150;
+        int x2 = x1 + (int) (100.0*Math.sin(anguloVela));
+        int y2 = y1 + (int) (100.0*Math.cos(anguloVela));
+        if (focusVela){
+            g2.setColor(Color.red);
+            g2.drawLine(x1, y1, x2, y2);
+            g2.setColor(Color.black);
+        }
+        else{
+            g2.drawLine(x1, y1, x2, y2);
+        }
+        x1 = 150;
+        y1 = 300;
+    }
+}
