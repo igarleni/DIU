122be29c738e2d0004bb8f42b9bfe137664972b6
italogar6 <italogar6@hotmail.com>
2015-11-16 19:58:43 +0000

    Practica 7 completa

---
+++ src/Practica7/ImageFilter.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica7;
+
+/**
+ *
+ * @author Italo
+ */
+import Practica6.*;
+import java.io.File;
+import javax.swing.filechooser.*;  
+
+public class ImageFilter extends FileFilter {
+        
+    @Override
+    public boolean accept(File f) {         
+        if (f.isDirectory()) {             
+            return true;
+        }         
+        String filename = f.getName();
+        int dot = filename.lastIndexOf('.');
+        String extension = filename.substring(dot + 1);
+  
+        if (extension != null) {             
+            if( extension.equals("tiff") ||                 
+                extension.equals("tif") ||                 
+                extension.equals("gif") ||                 
+                extension.equals("jpeg") ||                 
+                extension.equals("jpg") ||                 
+                extension.equals("png")) {                     
+                return true;             
+            } else {                 
+                return false;             
+            }        
+        }           
+        return false;     
+    }       
+    @Override
+    public String getDescription() {         
+        return "Ficheros de Imagenes";     
+    } 
+}
